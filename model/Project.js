44a89ae543b32a68fdb71a0eea6492858f673614
calvin <puram.calvin@gmail.com>
2020-02-11 16:21:29 +0100

    created project model

---
+++ model/Project.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const ProjectSchema = new Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Users'
+  },
+  title: {
+    type: String,
+    required: [true, 'a title is required']
+  },
+  description: {
+    type: String,
+    required: [true, 'a project require description']
+  },
+  due: {
+    type: Date,
+    required: [true, 'a due date is required']
+  },
+  remark: {
+    type: String,
+    enum: {
+      values: ['Ongoing', 'Over-due', 'complete'],
+      message: 'only ongoing, over-due, complete is required'
+    }
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('Projects', ProjectSchema);
